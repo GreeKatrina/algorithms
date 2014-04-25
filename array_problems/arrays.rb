cf57f1da524cc5d57b7e7f3a689e408ea2b61247
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-04-25 13:55:53 -0500

    Returns middle element for arrays of odd size. - 5 failures

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -14,10 +14,14 @@ module ArrayUtil
   end
 
   def self.middle_element(array)
-    if array.length > 0
+    if array.length == 1
       array[0]
-    else
+    elsif array.length == 0
       nil
+    elsif array.length % 2 == 0
+      middle = (array[(array.length/2)+1] + array[(array.length/2)+1])/2
+    else
+      array[array.length/2]
     end
   end
 
